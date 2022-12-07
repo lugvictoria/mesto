a5490eeebbe5824d4ead39bcc539a94226b29302
lugvictoria <victory.senty@yandex.ru>
2022-12-07 19:19:52 +0300

    add

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,27 @@
 let popup = document.querySelector('.popup');
 let form = document.querySelector('.popup__container');
-let buttonEdit = document.querySelector('.profiles__buttons-edit');
+let buttonEdit = document.querySelector('.profiles__buttons-pen');
 let buttonSave = document.querySelector('.popup__form');
 let popupClose = document.querySelector('.popup__buttons-close');
-let inputName = document.querySelector('.popup__inputs_type_name');
-let inputHobby = document.querySelector('.popup__inputs_type_hobby');
+let inputName = document.querySelector('.popup__inputs_name');
+let inputProffession = document.querySelector('.popup__inputs_proffession');
 let gname = document.querySelector('.profiles__name');
-let hobby = document.querySelector('.profiles__subtitle');
+let proffession = document.querySelector('.profiles__subtitle');
 
 // функция для открытия popup
 function openPopup() {
   popup.classList.add('popup_opened');
   inputName.value = gname.textContent;
-  inputHobby.value = hobby.textContent;
+  inputProffession.value = proffession.textContent;
 }
 // функция работы с формой
 function formSave(evt) {
   evt.preventDefault();
   gname.textContent = inputName.value;
-  hobby.textContent = inputHobby.value;
+  proffession.textContent = inputProffession.value;
   closePopup();
 }
-// функция для  закрытия popup
+// функция для закрытия popup
 function closePopup() {
   popup.classList.remove('popup_opened');
 }
