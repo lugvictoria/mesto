56bbe412e202fa440efb559e2f74e36f2015abbc
lugvictoria <victory.senty@yandex.ru>
2022-12-10 20:50:42 +0300

    2 ревью 4 спринт

--- scripts/index.js
+++ scripts/index.js
@@ -1,31 +1,30 @@
 let popup = document.querySelector('.popup');
 let form = document.querySelector('.popup__container');
 let buttonEdit = document.querySelector('.profile__button-edit');
-let buttonSave = document.querySelector('.popup__form');
 let popupClose = document.querySelector('.popup__button-close');
-let inputName = document.querySelector('.popup__inputs_name');
-let inputProffession = document.querySelector('.popup__inputs_proffession');
-let gname = document.querySelector('.profile__title');
-let proffession = document.querySelector('.profile__subtitle');
+let inputName = form.querySelector('.popup__inputs_name');
+let inputProfession = form.querySelector('.popup__inputs_profession');
+let profileTitle = document.querySelector('.profile__title');
+let userProfession = document.querySelector('.profile__subtitle');
 
 // функция для открытия popup
 function openPopup() {
-  popup.classList.add('popup_opened');
-  inputName.value = gname.textContent;
-  inputProffession.value = proffession.textContent;
+  inputName.value = profileTitle.textContent;
+  inputProfession.value = userProfession.textContent;
+  popup.classList.add('popup__opened');
 }
 // функция работы с формой
 function formSave(evt) {
   evt.preventDefault();
-  gname.textContent = inputName.value;
-  proffession.textContent = inputProffession.value;
+  profileTitle.textContent = inputName.value;
+  userProfession.textContent = inputProfession.value;
   closePopup();
 }
 // функция для закрытия popup
 function closePopup() {
-  popup.classList.remove('popup_opened');
+  popup.classList.remove('popup__opened');
 }
 
 buttonEdit.addEventListener('click', openPopup);
-buttonSave.addEventListener('submit', formSave);
+form.addEventListener('submit', formSave);
 popupClose.addEventListener('click', closePopup);
