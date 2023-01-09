fbe5579974414e354873f3fab53b9aa45068da0a
lugvictoria <victory.senty@yandex.ru>
2023-01-09 14:30:29 +0300

    2.3 версия проекта 5 спринта

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,188 @@
+/** Элементы страницы */
+const profileEditButton = document.querySelector('.profile__button-edit');
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__subtitle');
+
+const newCardButton = document.querySelector('.profile__button-add');
+const cardsContainer = document.querySelector('.elements');
+const cardTemplate = document.getElementById('cards').content;
+
+const profileEditPopup = document.getElementById('popup_profile-edit');
+const profileEditForm = profileEditPopup.querySelector('.popup__button-save');
+const profileNameInput = profileEditPopup.querySelector('.popup__inputs_data_name');
+const profileJobInput = profileEditPopup.querySelector('.popup__inputs_data_profession');
+
+const newCardPopup = document.getElementById('popup_card-add');
+const newCardForm = newCardPopup.querySelector('.popup__form');
+const newCardSaveButton = newCardPopup.querySelector('.popup__button-save');
+const newCardTitle = newCardPopup.querySelector('.popup__inputs_place_name');
+const newCardLink = newCardPopup.querySelector('.popup__inputs_place_link');
+
+const imagePopup = document.getElementById('popup-picture');
+const imagePopupFigure = imagePopup.querySelector('.popup-picture__image');
+const imagePopupCaption = imagePopup.querySelector('.popup-picture__title');
+
+const popupCloseButtons = document.querySelectorAll('.popup__button-close');
+
+/** Функция добавляет карточку/карточки на страницу
+ *
+ * Аргументы:
+ * - контейнер для вставки,
+ * - один или несколько объектов с карточкой (при вставке массива с объектами использовать spread-оператор, например: ...arrayOfObjects)
+ *
+ * Ожидаемый формат объекта карточки:
+ * {  name: Строка с именем объекта (заголовок карточки),
+ *    link: Строка с полным адресом изображения   }
+ *
+ * Шаблон карточки для генерации:
+ * блок <template id="cards">
+ */
+function renderCards (container, ...cards) {
+  cards.forEach( card => {
+    container.prepend( getNewCard(card.name, card.link) );
+  });
+}
+
+/** Функция создает из шаблона элемент с новой карточкой и возвращает его */
+ function getNewCard (name, link) {
+  // Создание элемента из шаблона
+  const card = cardTemplate.querySelector('.card').cloneNode(true);
+
+  // Заполнение содержимого
+  card.querySelector('.card__image').src = link;
+  card.querySelector('.card__image').alt = name;
+  card.querySelector('.card__title').textContent = name;
+
+  // Обработчики нажатий
+  card.querySelector('.card__image').addEventListener('click', showImagePopup);
+  card.querySelector('.card__like-button').addEventListener('click', likeCard);
+  card.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+  return card;
+}
+
+/** Функция нажатия на лайк */
+function likeCard (event) {
+  event.target.closest('.card__like-button').classList.toggle('card__like-button_active');
+}
+
+/** Функция удаления карточки при нажатии на кнопку */
+function deleteCard (event) {
+  event.target.closest('.card').remove();
+}
+
+/** Функция открывает нужный попап */
+function openPopup (popup) {
+  popup.classList.add('popup_opened');
+}
+
+/** Функция закрывает текущий попап */
+function closePopup (event) {
+  event.target.closest('.popup').classList.remove('popup_opened');
+}
+
+/** Функция сохраняет введенные данные и закрывает попап */
+function saveProfileInfo (event) {
+  event.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileJob.textContent = profileJobInput.value;
+  closePopup (event);
+}
+
+/** Функция сохраняет введенные данные и закрывает попап */
+function saveNewCard (event) {
+  event.preventDefault();
+  const card = {
+    name: newCardTitle.value,
+    link: newCardLink.value
+  };
+  renderCards(cardsContainer, card);
+  closePopup(event);
+  newCardForm.reset();
+}
+
+/** Функция открывает попап с увеличенной картинкой */
+function showImagePopup (event) {
+  imagePopupFigure.src = event.target.src;
+  imagePopupFigure.alt = event.target.alt;
+  imagePopupCaption.textContent = event.target.closest('.card').querySelector('.card__title').textContent;
+  console.log(profileName.textContent);
+  openPopup(imagePopup);
+}
+
+/** Обработчики событий */
+profileEditButton.addEventListener('click', function () {
+  profileNameInput.value = profileName.textContent;
+  profileJobInput.value = profileJob.textContent;
+  openPopup(profileEditPopup);
+});
+profileEditForm.addEventListener('submit', saveProfileInfo );
+
+
+newCardButton.addEventListener('click', function () {
+  openPopup(newCardPopup);
+});
+
+newCardForm.addEventListener('submit', saveNewCard);
+
+popupCloseButtons.forEach(button => button.addEventListener('click', closePopup));
+
+/** Отобразить исходные карточки при загрузке страницы */
+renderCards(cardsContainer, ...initialCards);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // определение констант popup'ов по ID
 const popupEditProfile = document.getElementById('popup_profile-edit');
 const popupAddCard = document.getElementById('popup_card-add');
@@ -29,7 +214,7 @@ const popupPictureContainer = document.querySelector('.popup-picture__container'
 const cardContainer = document.querySelector('.elements');
 const userTemplate = document.querySelector('#template').content;
 const elementCard = userTemplate.querySelector('.card').cloneNode(true);
-
+const card = userTemplate.querySelector('.card').cloneNode('true');
 // функция для открытия popup'ов
 function openPopup(popup) {
   popup.classList.add('popup_opened');
@@ -94,6 +279,18 @@ function openPopupAddCard() {
   });
 }
 
+card.querySelector('.card__image').addEventListener('click', showImagePopup);
+const imagePopupFigure = popupImage.querySelector('.popup_picture__image');
+const imagePopupCaption = popupImage.querySelector('.popup-picture__title');
+
+function showImagePopup (event) {
+  imagePopupFigure.src = event.target.src;
+  imagePopupFigure.alt = event.target.alt;
+  imagePopupCaption.textContent = event.target.closest('.card').querySelector('.card__title').textContent;
+
+  openPopup(popupImage);
+}
+
 // функция createCard и функция поставить лайк внутри
 function createCard(card) {
   const buttonLike = card.querySelector('.card__like-button');
@@ -101,18 +298,21 @@ function createCard(card) {
     buttonLike.classList.toggle('card__like-button_active');
   }
   buttonLike.addEventListener('click', createLike);
-  const cardImage = card.querySelector('.card__image');
-  function openPopupImage() {
-    document.querySelector('.popup-picture__image').src = card.querySelector('.card__image').src;
-    document.querySelector('.popup-picture__title').textContent = card.querySelector('.card__title').textContent;
 
+  const cardImage = card.querySelector('.card__image');
+ /* cardImage.addEventListener('click', openPopupImage);
+  function openPopupImage() {
     popupImage.classList.toggle('popup_opened');
+  popupImage.querySelector('.popup-picture__image').src = card.querySelector('.card__image').src;
+    popupImage.querySelector('.popup-picture__title').textContent = card.querySelector('.card__title').textContent;
+
+
 
     buttonClosePicture.addEventListener('click', () => {
       popupImage.classList.remove('popup_opened');
     });
   }
-  cardImage.addEventListener('click', openPopupImage);
+
   const buttonDelete = card.querySelector('.card__delete-button');
   function removeCard() {
     card.remove();
@@ -129,3 +329,13 @@ buttonCloseProfile.addEventListener('click', closePopup(popupEditProfile));
 buttonCloseCard.addEventListener('click', closePopup(popupAddCard));
 buttonClosePicture.addEventListener('click', closePopup(popupImage));
 popupEditProfileContainer.addEventListener('submit', EditProfile);
+
+/* buttonClosePicture.addEventListener('click', closePopup(popupImage));;*/
+
+/*
+function openPopupImage2 (evt) {
+  openPopup(popupImage);
+  cardImage.src = evt.target.src;
+  cardImage.alt = evt.target.alt.textContent;
+  cardSubtitle.textContent = evt.target.alt;
+}*/
